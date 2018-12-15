25dcdb37bcef16c969c6a191dbe42343e3349b8d
jisoo <jisoo1170@naver.com>
2018-12-15 11:08:13 +0900

    refrigerator

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.db import IntegrityError
+import datetime
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def add(request):
+    return render(request, 'main/add.html')
