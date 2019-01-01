d6b00c20b1a31591655dde34650509854469f865
jisoo <jisoo1170@naver.com>
2019-01-01 16:46:12 +0900

    food count, delete http response redirect

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     url(r'main/(?P<user_id>\d+)$', index, name="index"),
 
-    url(r'food/count/(?P<food_id>\d+)$', food_count, name="food_count"),
-    url(r'food/delete/(?P<food_id>\d+)$', food_delete, name="food_delete"),
+    url(r'(?P<user_id>\d+)/food/count/(?P<food_id>\d+)$', food_count, name="food_count"),
+    url(r'(?P<user_id>\d+)/food/delete/(?P<food_id>\d+)$', food_delete, name="food_delete"),
     # url(r'(?P<user_id>\d+)/food/added/$', food_added, name="food_added"),
     # url(r'(?P<user_id>\d+)/food/expiary_date/(?P<food_id>\d+)$', food_expiary, name="food_expiray"),
     # url(r'(?P<user_id>\d+)/food/created_at/(?P<food_id>\d+)$', food_created, name="food_created"),
--- main/views.py
+++ main/views.py
@@ -5,6 +5,8 @@ from django.db import IntegrityError
 import datetime
 import json
 from django.core.serializers.json import DjangoJSONEncoder
+from django.core.urlresolvers import reverse
+
 
 def index(request, user_id):
     one_user = User.objects.get(pk=user_id)
@@ -14,14 +16,16 @@ def index(request, user_id):
     data =  json.dumps(data, cls=DjangoJSONEncoder, ensure_ascii = False)
     return HttpResponse(data, content_type='application/json')
 
-def food_count(request, food_id):
+def food_count(request, user_id, food_id):
     # count = request.Post.get("count")
-    count=1
+    count=3
     food.objects.filter(pk=food_id).update(count=count)
+    # return JsonResponse({'status' : 'success', 'user_id' : user_id})
+    return HttpResponseRedirect(reverse('main:index', kwargs={'user_id': user_id}))
 
-    return JsonResponse({'status' : 'success'})
 
-def food_delete(request, food_id):
+def food_delete(request, user_id, food_id):
     food.objects.filter(pk=food_id).update(status=3)
+    # return JsonResponse({'status' : 'success', 'user_id' : user_id})
+    return HttpResponseRedirect(reverse('main:index', kwargs={'user_id': user_id}))
 
-    return JsonResponse({'status' : 'success'})
